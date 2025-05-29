a2fd0a2410a78ba82975dac731ae211cff184de7
Muhammad Neizam Muarif <75818538+nezastore@users.noreply.github.com>
2025-05-30 01:29:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "Menginstall dependencies..."
 pip3 install -r requirements.txt
 
 # Menyelesaikan setup untuk Google Sheets API (hanya jika belum tersetup)
-echo "Pastikan kamu sudah punya file credentials JSON untuk Google Sheets (dataiplinode-1df59f53d098.json)"
+echo "Pastikan kamu sudah punya file credentials JSON untuk Google Sheets (dataiplinode-acdf604f102e.json)"
 echo "Tempatkan file credentials tersebut di direktori yang sama dengan script ini."
 
 echo "Penginstalan selesai!"
